cbf1b78b662dda3bfb2f9074a31bda9d01d715be
wf <mailforwf@gmail.com>
2015-07-11 14:32:31 +0500

    nginx

--- zapretnginx.py
+++ zapretnginx.py
@@ -32,7 +32,7 @@ def __genereate():
         # edr_ports = set(['443' if urlparse(i[0]).scheme == 'https' else '80' for i in edr_urls if i[0]])
         edr_ports = set([urlparse(i[0]).scheme for i in edr_urls if i[0]])
         for edr_port in edr_ports:
-            cur.execute("SELECT url FROM edrdata WHERE disabled=0 and domain=%(domain)s and url like '%(scheme)s;",
+            cur.execute("SELECT url FROM edrdata WHERE disabled=0 and domain=%(domain)s and url like %(scheme)s;",
                         {'domain': edr_domain, 'scheme': edr_port+'%'})
             edr_urls = cur.fetchall()
             conf_server = """server {
