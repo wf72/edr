63c9a1400cc886e8d8a063ad85dbd6b19f71a297
wf <mailforwf@gmail.com>
2015-07-11 14:06:53 +0500

    nginx

--- zapretnginx.py
+++ zapretnginx.py
@@ -3,6 +3,7 @@
 __author__ = 'wf'
 
 from urlparse import urlparse
+from urllib import quote_plus
 
 import zapretinfo_run as __edr
 
@@ -45,7 +46,7 @@ def __genereate():
             conf_location += """    location %s {
         proxy_pass http://127.0.0.1;
                 }
-""" % (edr_url.path if edr_url.path else "/")
+""" % (quote_plus(edr_url.path) if edr_url.path else "/")
         if not domain_block:
             conf_location += """    location / {
         proxy_pass http://$host;
