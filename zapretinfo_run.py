0d8b061c07622fba81667ea5923e93d44fa2a571
wf <mailforwf@gmail.com>
2017-03-02 10:43:34 +0500

    nginx convert test

--- zapretinfo_run.py
+++ zapretinfo_run.py
@@ -35,10 +35,13 @@ def idnaconv(url, reverse=False):
     if tmp_url:
         printt("Converting: %s, type %s" % (tmp_url, type(tmp_url)))
         if reverse:
+            printt("Result: %s" % tmp_url.strip().decode('idna'))
             return tmp_url.strip().decode('idna')
         else:
+            printt("Result: %s" % tmp_url.strip().decode('utf-8').encode('idna'))
             return tmp_url.strip().decode('utf-8').encode('idna')
     else:
+        printt("Result: %s" % tmp_url)
         return tmp_url
 
 
