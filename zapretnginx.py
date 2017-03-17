d52eabf946fbb9773b1b3a7c037d3ea9f77d84c7
wf <mailforwf@gmail.com>
2017-03-17 21:20:41 +0500

    несколько IP в одной записи в dump

--- zapretnginx.py
+++ zapretnginx.py
@@ -32,17 +32,17 @@ def __genereate():
         edr_urls = cur.fetchall()
         cur.execute("SELECT url FROM edrdata WHERE disabled=0 and url like %s;", ('%://' + edr_domain,))
         edr_urls += cur.fetchall()
-        try:
-            cur.execute("SELECT url FROM edrdata WHERE disabled=0 and url like %s;",
-                        ('%://' + __edr.idnaconv(edr_domain, True) + '/%',))
-            edr_urls += cur.fetchall()
-            cur.execute("SELECT url FROM edrdata WHERE disabled=0 and url like %s;",
-                        ('%://' + __edr.idnaconv(edr_domain, True),))
-            edr_urls += cur.fetchall()
-        except UnicodeDecodeError as e:
-            print("Cannot parse %s with error %s" % (edr_domain, e))
-        except UnicodeEncodeError as e:
-            print("Cannot parse %s with error %s" % (edr_domain, e))
+        # try:
+        #     cur.execute("SELECT url FROM edrdata WHERE disabled=0 and url like %s;",
+        #                 ('%://' + __edr.idnaconv(edr_domain, True) + '/%',))
+        #     edr_urls += cur.fetchall()
+        #     cur.execute("SELECT url FROM edrdata WHERE disabled=0 and url like %s;",
+        #                 ('%://' + __edr.idnaconv(edr_domain, True),))
+        #     edr_urls += cur.fetchall()
+        # except UnicodeDecodeError as e:
+        #     print("Cannot parse %s with error %s" % (edr_domain, e))
+        # except UnicodeEncodeError as e:
+        #     print("Cannot parse %s with error %s" % (edr_domain, e))
 
         edr_ports = sorted(set([urlparse(i[0].strip()).scheme for i in edr_urls if i[0]]))
         conf_ports = ''
