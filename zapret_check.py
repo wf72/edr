0d92d48d850c9aaa678efb515055aa83787b9986
wf <mailforwf@gmail.com>
2017-03-22 17:40:17 +0500

    multiprocess

--- zapret_check.py
+++ zapret_check.py
@@ -7,6 +7,7 @@ import urllib2
 import csv
 import zapretinfo_run as __edr
 from datetime import datetime
+from multiprocessing.dummy import Pool as ThreadPool
 
 
 def __start():
@@ -29,14 +30,33 @@ def zabbix_check_status_write(status):
         zb_file.close()
 
 
+def checksite(url):
+    result = {'notblocked': [], 'errors': [], 'blocked': []}
+    if url:
+        if url[:4] != "http":
+            url = "http://%s" % url
+        try:
+            answer = urllib2.urlopen(url, timeout=int(__edr.config('Main')['check_timeout']))
+            tmpanswer = answer.read()
+            if max(word in tmpanswer for word in __edr.config('Main')['find_words'].split("|")):
+                result['blocked'] = url
+            else:
+                result['notblocked'] = url
+        except urllib2.URLError:
+            result['errors'] = url
+        except socket.timeout:
+            result['errors'] = url
+    return result
+
+
 def checkblockedsites():
     """Возвращает 1, если есть не заблокированные сайты. Используется для zabbix."""
     __edr.LogWrite("Start check urls", "zb_check")
     f = urllib2.urlopen('http://api.antizapret.info/all.php?type=csv')
     reader = csv.reader(f, delimiter=';')
-    result = []
-    errors = []
-    blocked = []
+    pool = ThreadPool(4)
+    result = {'notblocked': [], 'errors': [], 'blocked': []}
+    urls = []
     count = 0
     max_count = int(__edr.config('Main')['max_url_check'])
     for row in reader:
@@ -48,31 +68,21 @@ def checkblockedsites():
         if url:
             if url[:4] != "http":
                 url = "http://%s" % url
-            try:
-                count += 1
-                answer = urllib2.urlopen(url, timeout=int(__edr.config('Main')['check_timeout']))
-                tmpanswer = answer.read()
-                if max(word in tmpanswer for word in __edr.config('Main')['find_words'].split("|")):
-                    blocked.append(url)
-                    continue
-                else:
-                    __edr.printt("Url %(url)s not blocked: \n===start====\n%(answer)s\n===end===\n" %
-                                 {"url": url, "answer": tmpanswer})
-                    __edr.LogWrite("Url %(url)s not blocked: \n===start====\n%(answer)s\n===end===\n" %
-                                   {"url": url, "answer": tmpanswer}, "zb_check")
-                    result.append(url)
-            except urllib2.URLError as e:
-                __edr.printt("There was an error: %r With: %s " % (e, url))
-                errors.append(url)
-            except socket.timeout as e:
-                __edr.printt("There was an error: %r With: %s " % (e, url))
-                errors.append(url)
-    __edr.printt("===\nBlocked result: %s\n" % blocked)
-    __edr.printt("===\nNot blocked result: %s\n" % result)
-    __edr.printt("===\nWith errors: %s\n" % errors)
-    __edr.LogWrite("===\nBlocked result: %s\n" % blocked)
-    __edr.LogWrite("===\nNot blocked result: %s\n" % result)
-    __edr.LogWrite("===\nWith errors: %s\n" % errors)
+            urls.append(url)
+            count += 1
+    results = pool.map(checksite, urls)
+    for i in results:
+        for key in result.keys():
+            if i[key]:
+                result[key].append(i[key])
+    __edr.printt("===\nBlocked result: %s\n" % result['blocked'])
+    __edr.printt("===\nNot blocked result: %s\n" % result['notblocked'])
+    __edr.printt("===\nWith errors: %s\n" % result['errors'])
+    __edr.LogWrite("===\nBlocked result: %s\n" % result['blocked'])
+    __edr.LogWrite("===\nNot blocked result: %s\n" % result['notblocked'])
+    __edr.LogWrite("===\nWith errors: %s\n" % result['errors'])
+    pool.close()
+    pool.join()
     zabbix_check_status_write(int(bool(result)))
     return int(bool(result))
 
